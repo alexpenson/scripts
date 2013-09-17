8159ff8a81aef15ae8b32613f91fb26662034656
Alex Penson <avp2106@columbia.edu>
2013-09-17 16:53:57 -0400

    left-align indels, require reference

--- SAVI_to_vcf.pl
+++ SAVI_to_vcf.pl
@@ -1,10 +1,24 @@
 #!/usr/bin/env perl
-### Converts SAVI report to CRAVAT format.
+### Converts SAVI report to VCF format.
+
+### samtools must be in your $PATH
+
 ### Assumes that deletions have been merged
 ### such that chr:pos looks like chr1:100000-100000
 
+### Indels are left-aligned to fit the VCF format:
+### One reference base to the left of each indel is part of both REF and ALT alleles
+
 use warnings;
 use strict;
+#use Bio::DB::Fasta;
+use Getopt::Long;
+#Getopt::Long::Configure(qw{no_auto_abbrev no_ignore_case_always});
+
+my $reference="";
+GetOptions ('f=s' => \$reference); # genome reference: faidx-indexed fasta
+#print "$reference\n";
+die "Usage: $0 -f genome.fa" unless length $reference;
 
 my $header = <<EOF;
 ##fileformat=VCFv4.1
@@ -15,12 +29,13 @@ my $header = <<EOF;
 ##FORMAT=<ID=ABQ,Number=1,Type=Integer,Description="Average quality of variant-supporting bases">
 ##FORMAT=<ID=AD,Number=1,Type=Integer,Description="Depth of variant-supporting reads">
 ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Total Read Depth">
+##FORMAT=<ID=FREQ,Number=1,Type=Integer,Description="Percentage of variant-supporting reads">
 #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	tumor	normal
 EOF
 
 print $header;
 
-while (<>) {
+while (<STDIN>) {
     chomp;
 
 #############################################
@@ -39,6 +54,26 @@ while (<>) {
 
     my ( $chr, undef , undef ) = split(/:|-/, $chr_pos);
 
+#############################################
+### LEFT-ALIGN INDELS
+
+    #############################################
+    ### INSERTIONS
+    if ($ref eq "-") {
+	my $ref_from_fasta = reference($chr_pos);
+	$ref = $ref_from_fasta;
+	$var = $ref_from_fasta . $var;
+    }
+    #############################################
+    ### DELETIONS
+    elsif ($var eq "-") {
+	$pos -= 1;
+	$chr_pos = "$chr:$pos-$pos";
+	my $ref_from_fasta = reference($chr_pos);
+	$ref = $ref_from_fasta . $ref;
+	$var = $ref_from_fasta;
+    }
+
 
 #############################################
 ### SAVI to VCF
@@ -50,11 +85,23 @@ while (<>) {
     my $QUAL = ".";
     my $FILTER = ".";
     my $INFO = "SAMPLE=$case;GENE=$gene;AA=$AA";
-    my $FORMAT = join ":", ("ABQ", "AD","DP");
-    my $tumor = join ":", ($t_qual, $t_vardepth, $t_totdepth);
-    my $normal = join ":", ($n_qual, $n_vardepth, $n_totdepth);
+    my $FORMAT = join ":", ("ABQ", "AD", "DP", "FREQ");
+    my $tumor = join ":", ($t_qual, $t_vardepth, $t_totdepth, $tf);
+    my $normal = join ":", ($n_qual, $n_vardepth, $n_totdepth, $nf);
 
 #############################################
 ### WRITE VCF
     print join "\t", ($CHROM, $POS, $ID, $REF, $ALT, $QUAL, $FILTER, $INFO, $FORMAT, $tumor, $normal), "\n";
+}
+
+sub reference {
+    my $chr_pos = shift;
+    my $cmd = "samtools faidx $reference $chr_pos |";
+    open(SAMTOOLS, $cmd);
+    my @lines = <SAMTOOLS>;
+    chomp(my $ref_from_fasta = $lines[1]);
+#    print($ref_from_fasta . "\t" . $ref . "\n");
+    close(SAMTOOLS);
+    return uc($ref_from_fasta);
+    
 }
