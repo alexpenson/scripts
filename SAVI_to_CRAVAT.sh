146505c9db6a0abc3600e1aae5200b06c0dd33c0
Alex Penson <avp2106@columbia.edu>
2013-09-26 21:12:20 -0400

    scripts

---
+++ SAVI_to_CRAVAT.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env perl
+my($case, $tf, $tf_lower, $tf_upper, $nf, $nf_lower, $nf_upper, $tf_nf, $tf_nf_lower, $tf_nf_upper, $t_qual, $t_vardepth, $t_totdepth, $n_qual, $n_vardepth, $n_totdepth, $pos, $ref, $var, $Nexon, $nsyn, $Nspl, $typeSNP, $Ntot, $chr_pos, $ref_var, $gene, $CCDS, $sense, $exon_N, $codon, $AA)
