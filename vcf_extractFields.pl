3bf6ce123f744012bc8a1baa0a74193a6d91855d
Alex Penson <avp2106@columbia.edu>
2013-10-14 18:06:57 -0400

    updated bam folder

--- vcf_extractFields.pl
+++ vcf_extractFields.pl
@@ -16,13 +16,13 @@ my @samples = ("b", "n1", "n2", "t1", "t2");
 #############################################
 ### PRINT HEADER LINE
 my @cols = ("SAMPLE", "CHROM", "POS", "ID", "REF", "ALT",
-	    "b_FREQ", "b_DP", "b_AFF",
-	    "n1_FREQ", "n1_DP", "n1_AFF", 
-	    "n2_FREQ", "n2_DP", "n2_AFF",
+	    "b_FREQ", "b_DP", "b_AFF", "b_GQ",
+	    "n1_FREQ", "n1_DP", "n1_AFF", "n1_GQ", 
+	    "n2_FREQ", "n2_DP", "n2_AFF", "n2_GQ",
 	    "normal_FREQ", "normal_DP",
-	    "t1_FREQ", "t1_DP", "t1_AFF",
-	    "t2_FREQ", "t2_DP", "t2_AFF", 
-	    "tumor_FREQ", "tumor_DP",
+	    "t1_FREQ", "t1_DP", "t1_AFF", "t1_QUAL",
+	    "t2_FREQ", "t2_DP", "t2_AFF", "t2_QUAL", 
+	    "tumor_FREQ", "tumor_DP", "tumor_QUAL",
 	    "EFFECT", "IMPACT", "FUNCLASS", "CODON", "AA", "AA_LEN", "GENE", "BIOTYPE", "CODING", "TRID", "EXON_RANK",
 	    "dbSNPBuildID", "COSMIC_NSAMP", "N_PATIENTS_NORMAL");
 print "#", join("\t", @cols), "\n";
@@ -77,6 +77,7 @@ foreach my $vcf_filename (@ARGV) {
 	$fields{ALT} = $$x{ALT}[0]; ### choose the primary alternate allele
 
 	foreach my $sample (@samples_in_file) {
+	    if ($$x{gtypes}{$sample}{PVAL} eq ".") { $$x{gtypes}{$sample}{PVAL} = 1 }; 
 	    if ($$x{gtypes}{$sample}{DP} eq ".") { $$x{gtypes}{$sample}{DP} = 0 }; 
 	    if ($$x{gtypes}{$sample}{FREQ} eq ".") { $$x{gtypes}{$sample}{FREQ} = 0 }; 
 	    if ($$x{gtypes}{$sample}{AD} eq ".") { $$x{gtypes}{$sample}{AD} = 0 }; 
@@ -84,6 +85,11 @@ foreach my $vcf_filename (@ARGV) {
 	    
 	    $fields{$sample."_FREQ"} = $$x{gtypes}{$sample}{FREQ};
 	    $fields{$sample."_DP"} = $$x{gtypes}{$sample}{DP};
+	    if (grep {$_ eq $sample} ("b","n1","n2")) {
+		$fields{$sample."_GQ"} = $$x{gtypes}{$sample}{GQ};
+	    } else {
+		$fields{$sample."_QUAL"} = -10 * log( $$x{gtypes}{$sample}{PVAL} ) / log(10);	       
+	    }
 	    $fields{$sample."_AFF"} = 0;
 	    if ($$x{gtypes}{$sample}{AD} > 0) {
 		$fields{$sample."_AFF"} = 100 * $$x{gtypes}{$sample}{ADF} / $$x{gtypes}{$sample}{AD};
@@ -105,6 +111,10 @@ foreach my $vcf_filename (@ARGV) {
 	    $fields{tumor_FREQ} = ($$x{gtypes}{t1}{FREQ} * $$x{gtypes}{t1}{DP} + $$x{gtypes}{t2}{FREQ} * $$x{gtypes}{t2}{DP}) / $fields{tumor_DP};
 	    $fields{tumor_FREQ} = sprintf("%.2f",$fields{tumor_FREQ})
 	}
+	$fields{tumor_QUAL} = $fields{t1_QUAL} + $fields{t2_QUAL};
+	$fields{t1_QUAL} = sprintf("%.1f", $fields{t1_QUAL});
+	$fields{t2_QUAL} = sprintf("%.1f", $fields{t2_QUAL});
+	$fields{tumor_QUAL} = sprintf("%.1f", $fields{tumor_QUAL});
 
 #############################################
 ### PARSE THE snpEff EFF FIELD
